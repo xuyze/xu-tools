d5affae75ad0c0cfe8782c9774fd922c8a6cdd91
xuyz <12510705@qq.com>
2018-06-11 23:57:31 +0800

    1

---
+++ src/main/java/TestTreeMap.java
@@ -0,0 +1,19 @@
+import java.util.TreeMap;
+
+/**
+ * 功能描述：
+ *
+ * @version 1.0.0
+ * @author: <a href="12510705@qq.com">Zeny</a>
+ * @Date: Created in 2018/6/11 23:46.
+ */
+public class TestTreeMap {
+
+    public static void main(String[] args){
+        TreeMap map = new TreeMap();
+        map.put(2,12);
+        map.put(1,1);
+        map.put(3,1);
+        System.out.print(map);
+    }
+}
